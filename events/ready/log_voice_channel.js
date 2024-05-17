0d5959b6d5079694e7e191ddae6d75e7478c1550
Chanakun <chanakun428@gmail.com>
2024-05-17 18:05:48 +0700

    awdawdawdwad

--- events/ready/log_voice_channel.js
+++ events/ready/log_voice_channel.js
@@ -1,7 +1,7 @@
 const { voiceStateUpdate, SlashCommandBuilder, EmbedBuilder, Client, ContextMenuCommandInteraction, CommandInteraction, ActivityType } = require('discord.js');
 module.exports = async (client) => {
     client.on('voiceStateUpdate', async (oldState, newState) => {
-        const channel = await client.channels.cache.get('1240932283242905621');
+        const channel = await client.channels.cache.get('1230400253115502664');
         const now = await new Date();
         const dateOptions = { year: 'numeric', month: 'long', day: '2-digit' };
         const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
@@ -9,14 +9,15 @@ module.exports = async (client) => {
         const timeFormatter = await new Intl.DateTimeFormat('th-TH', timeOptions);
         const formattedDate = dateFormatter.format(now);
         const formattedTime = timeFormatter.format(now);
+
         if (oldState.channelId !== newState.channelId && oldState.channelId != null && newState.channelId != null) {
-            const oldChannelName = oldState.channel ? oldState.channel.name : 'None';
-            const newChannelName = newState.channel ? newState.channel.name : 'None';
+            const oldChannelName = oldState.channel ? oldState.channel : 'None';
+            const newChannelName = newState.channel ? newState.channel : 'None';
             const icon_ = newState.guild.members.cache.get(newState.member.user.id);
             const embed = new EmbedBuilder()
                 .setAuthor({ name: `${newState.member.user.tag}| Switch`, iconURL: icon_.user.displayAvatarURL() })
                 .setColor('Orange')
-                .setDescription(`\`${newState.member.user.tag}\`|Switch to voice channel| \`${oldChannelName}\`➡️ \`${newChannelName}\``)
+                .setDescription(`${newState.member.user}|Switch to voice channel| ${oldChannelName}➡️ ${newChannelName}`)
                 .setFooter({ text: `${formattedDate} | ${formattedTime}   • Created by _nv23` })
             channel.send({ embeds: [embed] });
         }
@@ -27,7 +28,7 @@ module.exports = async (client) => {
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: `${newState.member.user.tag}| Left`, iconURL: icon_.user.displayAvatarURL() })
                     .setColor('Red')
-                    .setDescription(`\`${newState.member.user.tag}\` |Left from voice channel|➡️ \`${oldState.channel.name}\``)
+                    .setDescription(`${newState.member.user} |Left from voice channel|➡️ ${oldState.channel}`)
                     .setFooter({ text: `${formattedDate} | ${formattedTime}   • Created by _nv23` })
                 channel.send({ embeds: [embed] });
             }
@@ -36,9 +37,10 @@ module.exports = async (client) => {
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: `${oldState.member.user.tag} | Join`, iconURL: icon_.user.displayAvatarURL() })
                     .setColor('Green')
-                    .setDescription(`\`${oldState.member.user.tag}\` |Join voice channel|➡️ \`${newState.channel.name}\``)
+                    .setDescription(`${oldState.member.user} |Join voice channel|➡️ ${newState.channel}`)
                     .setFooter({ text: `${formattedDate} | ${formattedTime} • Created by _nv23` })
                 channel.send({ embeds: [embed] });
+                
             }
         }
     })
